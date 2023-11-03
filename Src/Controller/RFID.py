583fddcfc9c088353651b6289a4ddb3e34b14c3c
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2023-11-03 15:08:33 -0300

    Sistema em funcionamento
    Foi ajusta as informações no banco de dados para atualizar as informações de registros

--- Src/Controller/RFID.py
+++ Src/Controller/RFID.py
@@ -5,29 +5,30 @@ from confg import db
 
 class RFID:
     def Register(rfidCode):
-        # Grab date/time
-        sao_paulo = timezone("America/Sao_Paulo")
-        now = datetime.now(sao_paulo)
-        data = now.strftime("%d/%m/%Y")
-        hora = now.strftime("%H:%M:%S")
-
         # Filter user (Unique register)
         rfid = CartaoRFID.query.filter_by(numRfid=rfidCode).first()
+
         if rfid == None:
             print("Não existe cadastro")
-            return "0"
+            return "Não cadastrado"
         else:
+            # Grab date/time
+            sao_paulo = timezone("America/Sao_Paulo")
+            now = datetime.now(sao_paulo)
+            data = now.strftime("%d/%m/%Y")
+            hora = now.strftime("%H:%M:%S")
             # Filter rfid regiters
             query = Registro.query.filter_by(rfid=rfidCode, dt=data).all()
             status = (
                 "Saída"
                 if query != [] and query[-1].statusReg == "Entrada" else "Entrada"
             )
+
             # Create an obj of Register and add in DB
-            reg = Registro(rfidCode, data, hora, status)
+            reg = Registro(rfid.id, rfidCode, data, hora, status)
             db.session.add(reg)
             db.session.commit()
-            return "1"
+            return "Cadastrado"
 
     def List(page, _data, per_page=10):
         sao_paulo = timezone("America/Sao_Paulo")
@@ -35,7 +36,7 @@ class RFID:
         data = now.strftime("%d/%m/%Y")
         if _data == "None" or _data is None or len(_data) < 1:
             query = (
-                Registro.query.join(Usuarios, Registro.rfid == Usuarios.rfid)
+                Registro.query.join(Usuarios, Registro.id == Usuarios.id)
                 .add_columns(Usuarios.nome, Registro.dt, Registro.hr, Registro.statusReg)
                 .filter(Registro.dt == data)
                 .paginate(page=page, per_page=per_page)
@@ -43,7 +44,7 @@ class RFID:
         else:
             _dataFilter = datetime.strptime(_data, "%Y-%m-%d").strftime("%d/%m/%Y")
             query = (
-                Registro.query.join(Usuarios, Registro.rfid == Usuarios.rfid)
+                Registro.query.join(Usuarios, Registro.id == Usuarios.id)
                 .add_columns(Usuarios.nome, Registro.dt, Registro.hr, Registro.statusReg)
                 .filter(Registro.dt == _dataFilter)
                 .paginate(page=page, per_page=per_page)
