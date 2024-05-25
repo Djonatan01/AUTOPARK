cf8c570342a6dc005463d1b75e263b9c5fd4e121
Vinicius Daniel Brandão de Souza <151692354+Vinicius-Brandao21@users.noreply.github.com>
2024-05-25 17:09:01 -0300

    Feito a confirmaçao de reserva da vaga
    foi feito a confirmação para reservar a vaga.

--- Src/Controller/Users.py
+++ Src/Controller/Users.py
@@ -46,7 +46,8 @@ class UserController:
       query = Usuarios.query.paginate(page=page, per_page=per_page)
     else:
       query = Usuarios.query.filter(Usuarios.nome.like('%'+_userFilter+'%')).paginate(page=page, per_page=per_page)
-      queryCount = Usuarios.query.count()
+      queryCount = Usuarios.query.count().all()
+      print("--------------------------------",queryCount)
     return {
       "regUser": query,
       "count": queryCount,
